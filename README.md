b5c71a75707e071797baec217276aa7c6f72af89
kmejia-crawford <kmejia-crawford@unomaha.edu>
2021-11-17 10:36:36 -0600

    yfyjf,f

---
+++ README.md
@@ -0,0 +1,9 @@
+# ISQA3900_Project
+This GitHub repository is for a team django project.
+
+We are creating a e-commerce application for watches.
+Our watches will be based off of the name "Wear-A-Watch"
+By collaborating on Pycharm we will build this website.
+
+Our application will be deployed on PythonAnywhere for each sprint
+to display our progress.
---
+++ shop/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, re_path
+from . import views
+
+app_name = 'shop'
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    re_path(r'^home/$', views.home, name='home'),
+    path('product_list', views.product_list, name='product_list'),
+    path('<slug:category_slug>/', views.product_list, name='product_list_by_category'),
+    path('<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
+    path('aboutus', views.aboutus, name='aboutus'),
+]
